35b8af1e6af817677fd7575bc7794d98009bb98d
Gerson <gerschacon@gmail.com>
2026-03-29 13:27:53 -0600

    Nugget

---
+++ Seguridad/Seguridad.API/BD/dbo/Stored Procedures/AgregarUsuario.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[AgregarUsuario]
+    @NombreUsuario     VARCHAR(MAX),
+    @PasswordHash      VARCHAR(MAX),
+    @CorreoElectronico VARCHAR(MAX)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @Id AS UNIQUEIDENTIFIER = NEWID();
+    BEGIN TRAN
+        INSERT INTO [dbo].[Usuarios]
+               ([Id], [NombreUsuario], [PasswordHash], [CorreoElectronico])
+        VALUES (@Id,  @NombreUsuario,  @PasswordHash,  @CorreoElectronico)
+
+        INSERT INTO [dbo].[PerfilesxUsuario]
+               ([IdUsuario], [IdPerfil])
+        VALUES (@Id, 1)
+
+        SELECT @Id
+    COMMIT TRAN
+END
